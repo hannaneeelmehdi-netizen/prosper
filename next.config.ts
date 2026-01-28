6fbc938c217297935b696ae489e407c450d89934
hannane elmehdi <hannaneelmehdi@gmail.com>
2026-01-28 22:29:14 +0000

    no need for databse i spoke with the client he said he will just receive

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  productionBrowserSourceMaps: false,
   typescript: {
     ignoreBuildErrors: true,
   },
